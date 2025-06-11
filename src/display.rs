46a6d5f1926b42527f7f9822608bcfcf173e14f3
Scott Fones <scott.fones@gmail.com>
2025-06-10 23:39:42 -0400

    removed SenseData struct. using libscd Measurement struct instead.

--- src/display.rs
+++ src/display.rs
@@ -3,32 +3,38 @@ use core::fmt::Write;
 use defmt::info;
 use embassy_time::Duration;
 use heapless::String;
+use libscd::measurement::Measurement;
 use microbit_bsp::display::{Bitmap, Frame, LedMatrix};
 use microbit_bsp::embassy_nrf::gpio::Output;
 
 use crate::buttons::{ButtonState, get_buttons_receiver};
-use crate::sense_i2c::{self, SenseData};
+use crate::sense_i2c;
 
 const COLS: usize = 5;
 const ROWS: usize = 5;
 
-fn set_dash(sense_data: &SenseData) -> [Bitmap; ROWS] {
+fn c_to_f(c: f32) -> f32 {
+    c * 9.0 / 5.0 + 32.0
+}
+
+fn set_dash(m: &Measurement) -> [Bitmap; ROWS] {
     let mut dash = [Bitmap::empty(COLS); ROWS];
+    let temp_f = c_to_f(m.temperature);
 
     // set bits from bottom -> top
     for (i, row) in dash.iter_mut().rev().enumerate() {
         // set tens digit for temp [60F-90F]
-        if (sense_data.get_temp_f() as usize) > (50 + 10 * i) {
+        if (temp_f as usize) > (50 + 10 * i) {
             row.set(0);
         }
 
         // set co2 bit in buckets of 200ppm starting at 400ppm
-        if (sense_data.get_co2() as usize) > (400 + 200 * i) {
+        if (m.co2 as usize) > (400 + 200 * i) {
             row.set(2);
         }
 
         // set humidity in bucket of 10%
-        if (sense_data.get_humid() as usize) >= (30 + 10 * i) {
+        if (m.humidity as usize) >= (30 + 10 * i) {
             row.set(4);
         }
     }
@@ -36,21 +42,21 @@ fn set_dash(sense_data: &SenseData) -> [Bitmap; ROWS] {
     // set bits from top -> bottom
     for (i, row) in dash.iter_mut().enumerate() {
         // set temperature secondary bit for every 2F
-        if (sense_data.get_temp_f() as usize) % 10 > (2 * i) {
+        if (temp_f as usize) % 10 > (2 * i) {
             row.set(1);
         }
 
         // set co2 secondary bit in buckets of 40ppm
-        if (sense_data.get_co2() as usize) % 200 > (40 * i) {
+        if (m.co2 as usize) % 200 > (40 * i) {
             row.set(3);
         }
     }
-
     dash
 }
 
-async fn display_dash(matrix: &mut LedMatrix<Output<'static>, ROWS, COLS>, sense_data: &SenseData) {
-    let dash = set_dash(sense_data);
+async fn display_dash(matrix: &mut LedMatrix<Output<'static>, ROWS, COLS>, m: &Measurement) {
+    let dash = set_dash(m);
+    matrix.set_brightness(microbit_bsp::display::Brightness::MIN);
     matrix
         .display(Frame::new(dash), Duration::from_millis(1000))
         .await;
@@ -64,6 +70,7 @@ async fn display_specific(
 ) {
     let mut disp_txt: String<9> = String::new();
     write!(&mut disp_txt, " {data} {units}").unwrap();
+    matrix.set_brightness(microbit_bsp::display::Brightness::MAX);
     matrix
         .scroll_with_speed(disp_txt.as_str(), Duration::from_millis(display_ms))
         .await;
@@ -78,24 +85,25 @@ pub async fn display_task(mut matrix: LedMatrix<Output<'static>, ROWS, COLS>) {
     let btn_rx = get_buttons_receiver();
     if let Some(mut sense_rx) = sense_i2c::get_sensor_receiver() {
         loop {
-            let sense_data = sense_rx.get().await;
+            let m = sense_rx.get().await;
 
             match btn_rx.try_receive() {
                 Ok(ButtonState::A) => {
                     info!("Button A: Display Temp F");
                     let (display_ms, units) = (2750, "F");
-                    display_specific(sense_data.get_temp_f(), display_ms, &mut matrix, units).await;
+                    let temp_f = c_to_f(m.temperature) as u16;
+                    display_specific(temp_f, display_ms, &mut matrix, units).await;
                     continue;
                 }
                 Ok(ButtonState::B) => {
                     info!("Button B: Display CO2 PPM");
                     let (display_ms, units) = (4500, "ppm");
-                    display_specific(sense_data.get_co2(), display_ms, &mut matrix, units).await;
+                    display_specific(m.co2, display_ms, &mut matrix, units).await;
                     continue;
                 }
                 // Only possible error is TryReceiveError, indicating an empty buffer
                 Err(_) => {
-                    display_dash(&mut matrix, &sense_data).await;
+                    display_dash(&mut matrix, &m).await;
                 }
             }
         }
--- src/sense_i2c.rs
+++ src/sense_i2c.rs
@@ -3,50 +3,16 @@ use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::watch::{DynReceiver, Watch};
 use embassy_time::{Delay, Timer};
 use libscd::asynchronous::scd4x::Scd4x;
+use libscd::measurement::Measurement;
 use microbit_bsp::embassy_nrf::peripherals::{P0_26, P1_00, TWISPI0};
 use microbit_bsp::embassy_nrf::twim::Twim;
 use microbit_bsp::embassy_nrf::{bind_interrupts, twim};
 
 const SENSE_CONSUMERS: usize = 1;
-static SENSOR_LENS: Watch<ThreadModeRawMutex, SenseData, SENSE_CONSUMERS> = Watch::new();
+static SENSOR_LENS: Watch<ThreadModeRawMutex, Measurement, SENSE_CONSUMERS> = Watch::new();
 
-pub fn get_sensor_receiver() -> Option<DynReceiver<'static, SenseData>> {
+pub fn get_sensor_receiver() -> Option<DynReceiver<'static, Measurement>> {
     SENSOR_LENS.dyn_receiver()
-}
-
-#[derive(Clone, Copy)]
-pub struct SenseData {
-    co2: u16,
-    humid: u16,
-    temp_c: u16,
-    temp_f: u16,
-}
-
-impl SenseData {
-    fn new(co2: u16, humid: f32, temp_c: f32, temp_f: f32) -> Self {
-        SenseData {
-            co2,
-            humid: (humid as u16),
-            temp_c: (temp_c as u16),
-            temp_f: (temp_f as u16),
-        }
-    }
-
-    pub fn get_co2(&self) -> u16 {
-        self.co2
-    }
-
-    pub fn get_humid(&self) -> u16 {
-        self.humid
-    }
-
-    pub fn get_temp_c(&self) -> u16 {
-        self.temp_c
-    }
-
-    pub fn get_temp_f(&self) -> u16 {
-        self.temp_f
-    }
 }
 
 #[embassy_executor::task]
@@ -76,16 +42,11 @@ pub async fn sense_i2c_task(twi: TWISPI0, sda: P1_00, scl: P0_26) {
             let m = scd.read_measurement().await.unwrap();
             let temp_f = m.temperature * 9.0 / 5.0 + 32.0;
 
-            let sense_data = SenseData::new(m.co2, m.humidity, m.temperature, temp_f);
-
             info!(
                 "CO2: {}, Humidity: {}, Temperature: {} ({})",
-                sense_data.get_co2(),
-                sense_data.get_humid(),
-                sense_data.get_temp_c(),
-                sense_data.get_temp_f()
+                m.co2, m.humidity as u16, m.temperature as u16, temp_f as u16
             );
-            tx.send(sense_data);
+            tx.send(m);
         }
         Timer::after_millis(5000).await;
     }
