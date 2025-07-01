2f82c2e73bca32c334f463c03ff0f93f9ad24911
Scott Fones <scott.fones@gmail.com>
2025-07-01 14:13:03 -0400

    moved steal into main. consolidated buttons into one task

--- src/buttons.rs
+++ src/buttons.rs
@@ -1,11 +1,10 @@
-use embassy_futures::select::{Either, select};
+use embassy_futures::select::{Either3, select3};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::channel::{Channel, DynamicReceiver, DynamicSender};
 use embassy_time::Timer;
 use microbit_bsp::Button;
 use microbit_bsp::embassy_nrf::Peri;
 use microbit_bsp::embassy_nrf::gpio::{AnyPin, Input, Pull};
-use microbit_bsp::embassy_nrf::peripherals::P1_04;
 
 static BUTTONS_LENS: Channel<ThreadModeRawMutex, ButtonState, 3> = Channel::new();
 
@@ -25,33 +24,32 @@ pub enum ButtonState {
 }
 
 #[embassy_executor::task]
-pub async fn buttons_task(mut btn_a: Button, mut btn_b: Button) {
+pub async fn buttons_task(
+    mut btn_a: Button,
+    mut btn_b: Button,
+    btn_touch_any: Peri<'static, AnyPin>,
+) {
+    let mut btn_touch = Input::new(btn_touch_any, Pull::None);
+
     let tx = get_buttons_sender();
     loop {
-        match select(btn_a.wait_for_falling_edge(), btn_b.wait_for_falling_edge()).await {
-            Either::First(_) => {
+        match select3(
+            btn_a.wait_for_falling_edge(),
+            btn_b.wait_for_falling_edge(),
+            btn_touch.wait_for_falling_edge(),
+        )
+        .await
+        {
+            Either3::First(_) => {
                 tx.send(ButtonState::A).await;
             }
-            Either::Second(_) => {
+            Either3::Second(_) => {
                 tx.send(ButtonState::B).await;
+            }
+            Either3::Third(_) => {
+                tx.send(ButtonState::C).await;
             }
         }
         Timer::after_millis(500).await;
-    }
-}
-
-#[embassy_executor::task]
-pub async fn touch_task() {
-    let tx = get_buttons_sender();
-    unsafe {
-        let touch_peri = P1_04::steal();
-        let touch_any: Peri<'static, AnyPin> = touch_peri.into();
-        let mut touch_input = Input::new(touch_any, Pull::None);
-
-        loop {
-            touch_input.wait_for_low().await;
-            tx.send(ButtonState::C).await;
-            Timer::after_millis(500).await;
-        }
     }
 }
